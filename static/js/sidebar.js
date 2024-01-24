4e4611b5a8f641b08ad24cf95ce7c0a106a2c182
Alexander Berryhill <amsb99@gmail.com>
2024-01-23 20:11:48 -0700

    Added Sidebar

---
+++ static/js/sidebar.js
@@ -0,0 +1,10 @@
+current_page = window.location.pathname.split("/").pop();
+
+function redirectToURL(link) {
+    console.log("Redirecting to:", link);
+    window.location.href = link;
+}
+
+function goBack() {
+    window.history.back();
+  }
